21525f32626ce3a40eff95928f9e96e41b030ba8
sridevi-sriagithub <sridevi@sriainfotech.com>
2025-09-25 17:03:43 +0530

    25/09/25 hosted code.

--- Tracking_BE/settings.py
+++ Tracking_BE/settings.py
@@ -122,6 +122,25 @@ DATABASES = {
         'PORT': '5433'
     }
 }
+import os
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+  
+
 
 
 # Password validation
